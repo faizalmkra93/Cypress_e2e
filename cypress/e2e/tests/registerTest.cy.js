70eae52cb6057d59f20cffa7f7e971cd7d3c9598
fmkfaizal <fmkfaizal@gmail.com>
2024-06-07 10:07:21 +0530

    Register test case have been created

---
+++ cypress/e2e/tests/registerTest.cy.js
@@ -0,0 +1,22 @@
+import { registerPage } from "../../pages/registerPage"
+const registerObj=new registerPage()
+import registerData from '../../fixtures/registerData.json'
+
+
+describe('Cypress POM suite1',()=>{
+
+    it('Register flow test',()=>{
+        registerObj.openURL()
+        registerObj.enterFirstName(registerData.firstName)
+        registerObj.enterlastName(registerData.lastName)
+        registerObj.enterEmail(registerData.email)
+        registerObj.enterTelephone(registerData.telephone)
+        registerObj.enterPassword(registerData.password)
+        registerObj.selectCheckbox()
+        registerObj.clickOnContinue()
+
+
+
+    })
+
+})
